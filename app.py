17540ff23ed8d058233e17e4cfb004ba0643382f
Ali Farsi <ali.farsi.nejad@gmail.com>
2023-09-01 19:17:03 -0400

    Added submit button

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from langchain import HuggingFaceHub
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 st.set_page_config(page_title="QA app", page_icon=":robot:")
 question = st.text_input("Enter your question")
+question = str(question)
 
-#model = HuggingFaceHub(repo_id = "/google/flan-t5-large")
+
 model = HuggingFaceHub(repo_id = "google/flan-t5-large")
-
-answer = model(question)
-st.write(answer)
+submit = st.button("Generate Answer")
+if submit:
+    answer = model(question)
+    st.write(answer)
